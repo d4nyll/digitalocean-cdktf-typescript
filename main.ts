3931403ce04dac462585c5532984bdbd66150613
Daniel Li <dan@danyll.com>
2022-06-26 13:27:56 +0100

    Define 2 Droplets running NGINX

--- main.ts
+++ main.ts
@@ -1,13 +1,33 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-import { DigitaloceanProvider } from "./.gen/providers/digitalocean"
+import { DataDigitaloceanSshKey, DigitaloceanProvider, Droplet } from "./.gen/providers/digitalocean"
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     new DigitaloceanProvider(this, 'provider')
-    
+
+    const dropletNames = ['foo', 'bar']
+    const sshKey = new DataDigitaloceanSshKey(this, 'sshKey', {
+      name: 'do_cdktf',
+    })
+    const droplets = dropletNames.map(name => new Droplet(this, name, {
+        image: 'ubuntu-20-04-x64',
+        name,
+        region: 'lon1',
+        size: 's-1vcpu-1gb',
+        sshKeys: [sshKey.id.toString()],
+        userData: `#!/bin/bash
+
+apt-get -y update
+apt-get -y install nginx
+export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+echo Droplet: $HOSTNAME, IP Address: $PUBLIC_IPV4 > /var/www/html/index.html
+`
+      })
+    )
   }
 }
 
