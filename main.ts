57e82ed684444a94771e7f364af6eecf983471c7
Daniel Li <dan@danyll.com>
2022-06-26 13:12:21 +0100

    Add DigitaloceanProvider to MyStack

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { DigitaloceanProvider } from "./.gen/providers/digitalocean"
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new DigitaloceanProvider(this, 'provider')
+    
   }
 }
 
